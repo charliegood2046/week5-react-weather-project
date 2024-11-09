801b3ae74823e0f58a1e4db29d9bebd55b3a1cd8
charliegood2046 <149835864+charliegood2046@users.noreply.github.com>
2024-11-09 14:40:05 -0600

    Added Bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import logo from "./logo.svg";
+import React from "react";
 
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by Charlie G and is hosted on{" "}
-        <a
-          href="https://github.com/charliegood2046/week5-react-weather-project"
-          target="_blank"
-        >
-          Github
-        </a>{" "}
-        and open-sourced.
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+
+        <footer>
+          This project was coded by Charlie G and is hosted on{" "}
+          <a
+            href="https://github.com/charliegood2046/week5-react-weather-project"
+            target="_blank"
+          >
+            Github
+          </a>{" "}
+          and open-sourced.
+        </footer>
+      </div>
     </div>
   );
 }
