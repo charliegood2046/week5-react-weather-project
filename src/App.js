a07e1dc62cb04231d85a9363f96bf8b7f303ed6b
charliegood2046 <149835864+charliegood2046@users.noreply.github.com>
2024-11-09 14:03:06 -0600

    Added Github repo URL in footer.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import logo from "./logo.svg";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by Charlie G and is hosted on{" "}
+        <a
+          href="https://github.com/charliegood2046/week5-react-weather-project"
+          target="_blank"
+        >
+          Github
+        </a>{" "}
+        and open-sourced.
+      </footer>
+    </div>
+  );
 }
