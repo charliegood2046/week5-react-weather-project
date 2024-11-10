5ebf497b143d293f8b47ee26d8ee41eb58f9a00d
charliegood2046 <149835864+charliegood2046@users.noreply.github.com>
2024-11-10 12:33:13 -0600

    Initial CSS styling.

---
+++ src/Weather.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Vienna</h1>
+      <ul>
+        <li>Saturday 22:00</li>
+        <li>Clear</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Clear"
+            />
+
+            <span className="temperature">4</span>
+            <span className="unit">°C</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 2%</li>
+            <li>Humidity: 80%</li>
+            <li>Wind: 5 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
