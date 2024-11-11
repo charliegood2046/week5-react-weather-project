10bc18d2aff7895a5024e217e5764796c04ced5b
charliegood2046 <149835864+charliegood2046@users.noreply.github.com>
2024-11-10 19:12:00 -0600

    added API integration.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1> Weather App</h1>
-        <Weather />
+        <Weather defaultCity="New York" />
 
         <footer>
           This project was coded by Charlie G and is hosted on{" "}
